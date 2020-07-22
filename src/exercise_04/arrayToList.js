66a0b4f275c147bacbc29675845212e154ad8977
Yang Siyu <siyuyang1995@gmail.com>
2020-07-22 23:35:26 +0800

    [yang siyu]feat: should throw if array is not defined or empty.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,13 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
 
   throw new Error('Please delete this line and implement the function');
 }

9d909185e36c63d47bbc48936a097397a0f934f7
Yang Siyu <siyuyang1995@gmail.com>
2020-07-27 18:58:21 +0800

    [yang siyu]feat: should throw if not all elements are promises

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,7 +9,7 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
   for (let i = 0; i < promises.length; i += 1) {
-    if (promises[i] instanceof Promise) {
+    if (!(promises[i] instanceof Promise)) {
       throw new Error('Not all elements are promises.');
     }
   }

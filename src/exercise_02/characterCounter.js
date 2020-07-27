366338ff400c7abdcc55032bb046220bc2ee06ec
Yang Siyu <siyuyang1995@gmail.com>
2020-07-27 15:34:09 +0800

    [yang siyu]feat: pass the exercise.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,11 +5,18 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-  //var reg = new RegExp(prediction, 'g');
-  //var result = string.match(reg);
   if (string === undefined || string == null || string.length === 0) {
     return 0;
   }
-  var count = prediction === null ? string.length:result.length;
-  return count;
+
+  if (prediction !== undefined) {
+    let count = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      if (prediction(string[i])) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+  return string.length;
 }
